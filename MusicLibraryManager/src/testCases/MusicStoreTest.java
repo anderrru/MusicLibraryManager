299f266ef76c0b69c01a88117de6d24d57158cba
Andrew Wong <andrewwong@Andrews-MacBook-Air.local>
2025-02-26 16:20:17 -0700

    test music store

--- MusicLibraryManager/src/testCases/MusicStoreTest.java
+++ MusicLibraryManager/src/testCases/MusicStoreTest.java
@@ -3,15 +3,20 @@ package testCases;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 
+import model.Album;
 import model.MusicStore;
 
 class MusicStoreTest {
 
 	@Test
-	void test(){
+	void testSearchByArtist() throws FileNotFoundException{
+		MusicStore store = new MusicStore();
+		ArrayList<Album> albums = store.searchAlbumByArtist("Adele");
+		assertEquals(albums.get(0).getTitle(), "19");
 	}
 
 }
